3f63a1010bedcc3c344875016598f82e4d4a95d1
Geanderson Souza Pessini de Oliveira <geanderson.oliveira@dataprev.gov.br>
2024-12-17 12:54:16 -0300

    Fix: Refatorar Definicao de tipos de papel

--- Business/ImagePdf.cs
+++ Business/ImagePdf.cs
@@ -11,127 +11,55 @@ namespace Business
         public static byte[] ImagemToPdf(byte[] file, string? tamanho, bool? paisagem) // metódo que usa itext para transformar e manipular o pdf
                                                                                        // parametros : file => a imagem | tamanho => tipo de papel | paisagem => orientação do papel
         {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                //Initialize the PDF document object.            
-                ImageData imageData = ImageDataFactory.Create(file);
-                PdfDocument pdfDocument = new PdfDocument(new PdfWriter(stream));
-                Document document = new Document(pdfDocument);
+            using var stream = new MemoryStream();
 
-                Image image = new Image(imageData);
+            var imageData = ImageDataFactory.Create(file);
 
-                if (tamanho == null || tamanho.ToUpper() != "AUTO") //tamanho (tipo de papel, pode vir padronizado (A4,A5 ...)) ou Tipo auto, entao a partir da imagem identifica o padrao de papel ideal
-                {
-                    document = new Document(pdfDocument);
-                    pdfDocument = TamanhoPapel.TipoPapel(tamanho, pdfDocument); //define o tipo de Papel que foi Pedido
-                }
-                else
-                {
-                    if (tamanho.ToUpper() == "AUTO") // implementa o tipo de papel, caso for pedido tamanho auto
+            using var pdfWriter = new PdfWriter(stream);
+            using var pdfDocument = new PdfDocument(pdfWriter);
+            var document = new Document(pdfDocument);
 
-                    {
+            var image = new Image(imageData);
 
-                        using (MemoryStream memoryStream = new MemoryStream())
-                        {
+            // Define tamanho de papel
+            DefineTamanhoPapel(tamanho, pdfDocument, image);
 
-                            PdfDocument pdfdoc = new PdfDocument(new PdfWriter(memoryStream)); //pdfdoc variavel para comparar com o tamanho da imagem, até chegar no padrao ideal, começando com tipo A5.
+            // Adiciona imagem ao documento
+            image.SetRelativePosition(0, 0, 0, 0);
+            document.Add(image);
 
-                            pdfdoc.SetDefaultPageSize(PageSize.A5);
-                            pdfDocument.SetDefaultPageSize(PageSize.A4);
-
-                            //Se o tamanho da imagem for maior/igual que uma pagina A5 => documento final será A4 (mínimo)
-
-                            if (image.GetImageScaledHeight() >= pdfdoc.GetDefaultPageSize().GetHeight() && image.GetImageScaledWidth() >= pdfdoc.GetDefaultPageSize().GetWidth())
-                            {
-                                pdfDocument.SetDefaultPageSize(PageSize.A4);
-                                pdfdoc.SetDefaultPageSize(PageSize.A4);
-
-                                //se o tamanho da imagem for maior/igual que pagina A4 => documento final será A3
-
-                                if (image.GetImageScaledHeight() > pdfdoc.GetDefaultPageSize().GetHeight() && image.GetImageScaledWidth() > pdfdoc.GetDefaultPageSize().GetWidth())
-                                {
-                                    pdfDocument.SetDefaultPageSize(PageSize.A3);
-                                    pdfdoc.SetDefaultPageSize(PageSize.A3);
-
-                                    //se o tamanho da imagem for maior/igual que pagina A3 => documento final será A2
-
-                                    if (image.GetImageScaledHeight() > pdfdoc.GetDefaultPageSize().GetHeight() && image.GetImageScaledWidth() > pdfdoc.GetDefaultPageSize().GetWidth())
-                                    {
-                                        pdfDocument.SetDefaultPageSize(PageSize.A2);
-                                        pdfdoc.SetDefaultPageSize(PageSize.A2);
-
-                                        //se o tamanho da imagem for maior/igual que pagina A2 => documento final será A1
-
-
-                                        if (image.GetImageScaledHeight() > pdfdoc.GetDefaultPageSize().GetHeight() && image.GetImageScaledWidth() > pdfdoc.GetDefaultPageSize().GetWidth())
-                                        {
-                                            pdfDocument.SetDefaultPageSize(PageSize.A1);
-                                            pdfdoc.SetDefaultPageSize(PageSize.A1);
-
-                                            //se o tamanho da imagem for maior/igual que pagina A1 => documento final será A0
-
-
-                                            if (image.GetImageScaledHeight() > pdfdoc.GetDefaultPageSize().GetHeight() && image.GetImageScaledWidth() > pdfdoc.GetDefaultPageSize().GetWidth())
-                                            {
-                                                pdfDocument.SetDefaultPageSize(PageSize.A0);
-                                                pdfdoc.SetDefaultPageSize(PageSize.A0);
-
-                                            } //Tipo A0 é o padrão de tamanho máximo
-
-                                        }
-
-                                    }
-                                }
-                            }
-                            pdfdoc.Close(); //encerra o pdfdoc, pois só serve para fazer as comparações
-                            memoryStream.Close();
-                        }
-
-                    }
-                }
-
-                if (image.GetImageHeight() >= image.GetImageWidth() || paisagem == false) //condicional para definir a orientação do documento, a partir da imagem, ou  se foi definido no parametro paisagem
-
-                {
-                    if (paisagem == true) // se paisagem for true, independente da imagem , o documento final será paisagem
-                    {
-                        var pdf = pdfDocument.GetDefaultPageSize();             // pegar TipoPapel para mudar orientação para paisagem
-                        pdfDocument.SetDefaultPageSize(pdf.Rotate());         //mudar orientação para paisagem
-
-                        if (image.GetImageScaledHeight() >= pdfDocument.GetDefaultPageSize().GetHeight())
-                        {
-                            image.SetAutoScale(true); // Metodo do itext que ajusta a imagem, caso ela ultrapasse os limites do papel
-                        }
-                    }
-                    else
-
-                    {
-                        if (image.GetImageScaledHeight() >= pdfDocument.GetDefaultPageSize().GetHeight()) { image.SetAutoScale(true); } //Auto Resize
-
-                    }
-
-                }
-                else
-                {
-                    var pdf = pdfDocument.GetDefaultPageSize();             // pegar TipoPapel para mudar orientação para paisagem
-                    pdfDocument.SetDefaultPageSize(pdf.Rotate());           //mudar orientação para paisagem
-
-                    if (image.GetImageScaledHeight() >= pdfDocument.GetDefaultPageSize().GetHeight() - 20 || image.GetImageScaledWidth() >= pdfDocument.GetDefaultPageSize().GetWidth() - 20)
-                    {
-                        image.SetAutoScale(true);
-                    }
-
-                }
-
-                //image.SetHorizontalAlignment(HorizontalAlignment.CENTER);
-                image.SetRelativePosition(0, 0, 0, 0); //seta a posição da imagem no documento ao canto superior esquerdo
-                document.Add(image);
-                pdfDocument.Close();
-
-                return stream.ToArray();
+            document.Close();
+            
+            return stream.ToArray();
 
             }
 
+        private static void DefineTamanhoPapel(string? tamanho, PdfDocument pdfDocument, Image image)
+        {
+            if (string.IsNullOrWhiteSpace(tamanho) || tamanho.ToUpper() == "AUTO")
+            {
+                AjustaTamanhoAuto(pdfDocument, image);
+            }
+            else
+            {
+                TamanhoPapel.TipoPapel(tamanho, pdfDocument);
+            }
         }
+
+        private static void AjustaTamanhoAuto(PdfDocument pdfDocument, Image image)
+        {
+            var tamanhos = new List<PageSize> { PageSize.A5, PageSize.A4, PageSize.A3, PageSize.A2, PageSize.A1, PageSize.A0 };
+
+            foreach (var tamanho in tamanhos)
+            {
+                pdfDocument.SetDefaultPageSize(tamanho);
+
+                if (image.GetImageScaledHeight() <= tamanho.GetHeight() && image.GetImageScaledWidth() <= tamanho.GetWidth())
+                {
+                    break; // Tamanho ideal encontrado
+                }
+            }
+        }
+
     }
-}
+    }
